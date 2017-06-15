7c7260286d21bdade47f3f0faf9ed10f3cc0fbf9
fisherthewol <george.kokinis@gmail.com>
2017-06-15 14:14:21 +0100

    Update main.py
    Generalisation! We're still working.

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import tdl
-__version__ = 1.01
+__version__ = 1.02
 # Set Window.
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 30
+
+
+class GameObject:
+    """Generic Object."""
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        """Move by given values"""
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        """Draw character representing object."""
+        con.draw_char(self.x, self.y, self.char, self.char, self.color)
+
+    def clear(self):
+        """Remove  character representing object at previous position."""
+        con.draw_char(self)
 
 
 def handle_keys():
@@ -24,8 +46,9 @@ def handle_keys():
         playerx += 1
 
 
-# Init console.
+# Init consoles.
 tdl.set_font("dejavu10x10.png", greyscale=True, altLayout=True)
+con = tdl.Console(SCREEN_WIDTH, SCREEN_HEIGHT)
 root = tdl.init(SCREEN_WIDTH,
                 SCREEN_HEIGHT,
                 title="Umbrella",
@@ -34,14 +57,16 @@ root = tdl.init(SCREEN_WIDTH,
 tdl.setFPS(LIMIT_FPS)
 
 # Set Player to centre.
-playerx = SCREEN_WIDTH//2
-playery = SCREEN_HEIGHT//2
+player = GameObject(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, "@", (255,255,255))
+npc = GameObject(SCREEN_WIDTH//2 - 5, SCREEN_HEIGHT//2, "@", (255,255,0))
+objects = [npc, player]
 
 # Main loop.
 while not tdl.event.is_window_closed():
-    console.draw_char(playerx, playery, '@', bg=None, fg=(255,255,255))
+    con.draw_char(playerx, playery, '@', bg=None, fg=(255,255,255))
+    root.blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0)
     tdl.flush()
-    console.draw_char(playerx, playery, ' ', bg=None)
+    con.draw_char(playerx, playery, ' ', bg=None)
     #handle keys and exit game if needed
     exit_game = handle_keys()
     if exit_game:
