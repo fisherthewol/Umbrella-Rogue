284c5faa893b8a9df4ff125ed38f2f67dda130d7
Fisher <george.kokinis@gmail.com>
2017-06-15 11:25:29 +0100

    Multiple changes
    Update main.py - fixed font
    Rename dejavu.png to dejavu10x10.png  - Font size needs to be in name so....

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ __version__ = 1.00
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 30
-tdl.set_font("dejavu.png", greyscale=True, altLayout=True)
+tdl.set_font("dejavu10x10.png", greyscale=True, altLayout=True)
 console = tdl.init(SCREEN_WIDTH,
                    SCREEN_HEIGHT,
                    title="Umbrella-Rogue",
                    fullscreen = False)
+tdl.setFPS(LIMIT_FPS)
+while not tdl.event.is_window_closed():
+    console.draw_char(1, 1, "@", bg=None, fg=(255,255,255))
+    tdl.flush()
