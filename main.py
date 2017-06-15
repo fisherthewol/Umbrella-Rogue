73f03ce4e92a953f1f29ec7bc7dd887f9a824850
Fisher <george.kokinis@gmail.com>
2017-06-15 11:20:36 +0100

    Multiple changes
    Update main.py - Begin loop.
    Create dejavu.png - Font.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import tdl
 __version__ = 1.00
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 30
+tdl.set_font("dejavu.png", greyscale=True, altLayout=True)
+console = tdl.init(SCREEN_WIDTH,
+                   SCREEN_HEIGHT,
+                   title="Umbrella-Rogue",
+                   fullscreen = False)
