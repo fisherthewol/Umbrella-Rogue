f1fe113569c4818b6ee9d056025b17262d3883e3
Fisher <george.kokinis@gmail.com>
2017-07-30 17:09:07 +0100

    Fixed #1
    Fixed #2; Changes class basicMonster.

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ class BasicMonster:
     def take_turn(self):
         monster = self.owner
         if (monster.x, monster.y) in visible_tiles:
-            if monster.distance_to(player) >= 2:
+            if monster.distance_to(player) >= 1.41:
                 monster.move_towards(player.x, player.y)
             elif player.fighter.hp > 0:
                 monster.fighter.attack(player)
